c316fb03bc885b00c1ef0575372dd87f9e9e9418
James Healy <james@yob.id.au>
2020-09-27 11:55:35 +1000

    bump to google-cloud-monitoring >= 1.0
    The google-cloud-monitoring gem changed it's interface in a non-backwards
    compatible way for the v1 release.
    
    This gem is a little side project, so rather than support both
    interfaces I'm just taking the quick option and updating to the
    new interface and bumping the dependency.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+0.2.0 (XXX)
+
+* require google-cloud-monitoring >= 1.0
+
 0.1.0 (27th September 2020)
 
 * fix dependencies to exclude google-cloud-monitoring >= 1.0
--- lib/stackdriver_simple.rb
+++ lib/stackdriver_simple.rb
@@ -18,24 +18,31 @@ class StackdriverSimple
     raise ArgumentError, "name cannot include /" if name.to_s.include?("/")
     raise ArgumentError, "value cannot be nil" if value.nil?
 
-    # This might have optional arguments for providing the project and credentials?
-    client = Google::Cloud::Monitoring::Metric.new
-    project_name = Google::Cloud::Monitoring::V3::MetricServiceClient.project_path(@google_cloud_project)
+    client = Google::Cloud::Monitoring.metric_service #do |config|
+    # The client can be initialised with a block to explicitly provide credentials. This
+    # gem is already published  with the assumption that credentials will be loaded automatically
+    # based on the GOOGLE_APPLICATION_CREDENTIALS env var, and I don't have the energy to change
+    # that behaviour for now.
+    # client = Google::Cloud::Monitoring.metric_service do |config|
+    #  config.credentials = "/path/to/credentials.json"
+    #end
 
-    series = Google::Monitoring::V3::TimeSeries.new
+    project_name = Google::Cloud::Monitoring::V3::MetricService::Paths.project_path(project: @google_cloud_project)
+
+    series = Google::Cloud::Monitoring::V3::TimeSeries.new
     metric = Google::Api::Metric.new type: "custom.googleapis.com/#{name}"
     series.metric = metric
 
     resource = Google::Api::MonitoredResource.new type: "global"
     series.resource = resource
 
-    point = Google::Monitoring::V3::Point.new
-    point.value = Google::Monitoring::V3::TypedValue.new double_value: value
+    point = Google::Cloud::Monitoring::V3::Point.new
+    point.value = Google::Cloud::Monitoring::V3::TypedValue.new double_value: value
     now = Time.now
     end_time = Google::Protobuf::Timestamp.new seconds: now.to_i, nanos: now.usec
-    point.interval = Google::Monitoring::V3::TimeInterval.new end_time: end_time
+    point.interval = Google::Cloud::Monitoring::V3::TimeInterval.new end_time: end_time
     series.points << point
 
-    client.create_time_series project_name, [series]
+    client.create_time_series name: project_name, time_series: [series]
   end
 end
